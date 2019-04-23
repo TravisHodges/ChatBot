526a26907bdec916768a12d54bf15278f53a573f
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2019-04-23 11:19:30 -0500

    a beginning

--- src/Greetings.java
+++ src/Greetings.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 public class Greetings
 {
+public static void main()
+{
+    Scanner input = new Scanner(System.in);
+    System.out.println();
 
+}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main
 {
-
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+    System.out.println("Hello! I am Morgan, and I'm a ChatBot! I'll talk to you whenever you boot up this program, and I have " +
+            "a bunch of stuff to say!");
+        System.out.println("Because of the 'restrictions' there are keywords to engage in a conversation with me. These keywords are:");
+        String Hi;
+    String userChoice;
+    userChoice = input.nextLine();
+    if (userChoice == Hi)
+        {
+        
+        }
+    }
 }
+
