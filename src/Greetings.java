f016d1beebd4f2440c29e22b8ee561176b8da249
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2019-04-24 11:20:08 -0500

    Greetings done, working on getting them to display when called in Main

--- src/Greetings.java
+++ src/Greetings.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 public class Greetings
 {
 public static void main()
@@ -6,5 +8,45 @@ public static void main()
     Scanner input = new Scanner(System.in);
     System.out.println();
 
+    ArrayList<String> Greetings = new ArrayList<String>();
+    for (int j = 0; j < 6; j++)
+    {
+       Random rand = new Random();
+       int n = rand.nextInt(6);
+       n += 1;
+       if (n == 1)
+       {
+        System.out.println("Hello!");
+       }
+       else if (n == 2)
+       {
+        System.out.println("Heyyyyy ;) (Is that how humans attract mates? Weird!");
+       }
+       else if (n == 3)
+       {
+        System.out.println("Hiya!");
+       }
+       else if (n == 4)
+       {
+        System.out.println("Hi.");
+       }
+       else if (n == 5)
+       {
+        System.out.println("Hey.");
+       }
+       else if (n == 6)
+       {
+        System.out.println("Wassup dawg?");
+       }
+       else
+           {
+               System.out.println("That isn't one of the programmed words, but I'll acknowledge that you spoke to me. :)");
+           }
+    }
+
 }
+    public static void displayGreetings(ArrayList<String> Greetings)
+    {
+    System.out.println(Greetings);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Main
 {
+    private static Object displayGreetings;
+
     public static void main(String[] args)
     {
+
         Scanner input = new Scanner(System.in);
     System.out.println("Hello! I am Morgan, and I'm a ChatBot! I'll talk to you whenever you boot up this program, and I have " +
             "a bunch of stuff to say!");
         System.out.println("Because of the 'restrictions' there are keywords to engage in a conversation with me. These keywords are:");
-        String Hi;
+        System.out.println("Hi, Tell me something, What do you want to talk about? (What do you want to talk about is a question for me, not you).");
+        String Hi = null;
     String userChoice;
     userChoice = input.nextLine();
     if (userChoice == Hi)
         {
-        
+            Object displayGreetings = Main.displayGreetings;
         }
     }
 }
